b241e3246d1eb3886b1efec4b2ddc242830b9192
Jakub Sistek <sistek@math.cas.cz>
2011-03-14 22:57:19 +0000

    licensing

--- src/module_adaptivity.f90
+++ src/module_adaptivity.f90
@@ -1,19 +1,18 @@
 ! BDDCML - Multilevel BDDC
-! Copyright (C) The BDDCML Team
 ! 
-! This program is free software: you can redistribute it and/or modify
-! it under the terms of the GNU General Public License as published by
-! the Free Software Foundation, either version 3 of the License, or
-! (at your option) any later version.
+! This program is a free software.
+! You can redistribute it and/or modify it under the terms of 
+! the GNU Lesser General Public License 
+! as published by the Free Software Foundation, 
+! either version 3 of the license, 
+! or (at your option) any later version.
 ! 
 ! This program is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-! GNU General Public License for more details.
-! 
-! You should have received a copy of the GNU General Public License
-! along with this program.  If not, see <http://www.gnu.org/licenses/>.
-! ____________________________________________________________________
+! GNU Lesser General Public License for more details
+! <http://www.gnu.org/copyleft/lesser.html>.
+!________________________________________________________________
 
 module module_adaptivity
 !***********************
