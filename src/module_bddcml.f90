31a817c4956c94d98932658d32330841d96bc78f
Jakub Sistek <sistek@math.cas.cz>
2025-04-11 16:48:58 +0200

    rely on the estimate from adaptive BDDC

--- src/module_bddcml.f90
+++ src/module_bddcml.f90
@@ -575,7 +575,7 @@ subroutine bddcml_solve(comm_all,method,tol,maxit,ndecrmax, &
          ! use Chebyshev iteration
          eigmin_bound = 1._kr
          ! estimate maximal eigenvalue as an estimate of the largest eigenvalue times safety factor
-         eigmax_bound = levels_max_eigenvalue * 1.2_kr
+         eigmax_bound = levels_max_eigenvalue
          call krylov_bddcchebyshev(comm_all,krylov_tol,krylov_maxit,krylov_ndecrmax, &
                                    krylov_recycling, krylov_max_number_of_stored_vectors, &
                                    eigmin_bound, eigmax_bound, &
